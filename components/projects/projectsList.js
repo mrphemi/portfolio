8c419fc2085fc0b3bb60874c962e54eb5d443e06
Kayode Oluwafemi <mrlapinho@gmail.com>
2020-05-02 15:19:53 +0100

    Update project live url and github links

--- components/projects/projectsList.js
+++ components/projects/projectsList.js
@@ -6,7 +6,7 @@ const projects = [
     img: "/assets/rest_countries.png",
     live: "//csb-wiknv.netlify.com",
     repo: "//github.com/mrphemi/rest_countries",
-    stack: ["javascript", "react", "CSS modules"]
+    stack: ["javascript", "react", "CSS modules"],
   },
   {
     title: "url shortener",
@@ -15,7 +15,7 @@ const projects = [
     img: "/assets/shortlyy.png",
     live: "//shortlyy.netlify.com",
     repo: "//github.com/mrphemi/url-shortener",
-    stack: ["javascript", "react", "sass"]
+    stack: ["javascript", "react", "sass"],
   },
   {
     title: "exploremoviez",
@@ -24,17 +24,17 @@ const projects = [
       "A movie explorer website created with Reactjs. Movies are fetched from the TMDB Api.",
     live: "//exploremoviez.netlify.com",
     repo: "//github.com/mrphemi/exploremoviez",
-    stack: ["javascript", "react", "redux", "scss"]
+    stack: ["javascript", "react", "redux", "scss"],
   },
   {
     title: "bantu studio",
     img: "/assets/bantu.png",
     description:
       "A digital studio portfolio. This is an unfinished project from my previous company.",
-    live: "//naitrons.github.io/bantustudio",
-    repo: "/",
-    stack: ["HTML", "CSS", "scss", "javascript", "jQuery"]
-  }
+    live: "//bantu-studio.netlify.app",
+    repo: "//github.com/mrphemi/bantustudio",
+    stack: ["HTML", "CSS", "scss", "javascript", "jQuery"],
+  },
 ];
 
 export default projects;
