b19841898f07bfcd81042078b9b9e1966bad944f
Femi <mrlapinho@gmail.com>
2024-10-26 22:49:01 +0100

    update portfolio

--- components/info/Info.js
+++ components/info/Info.js
@@ -1,40 +1,30 @@
 import React from "react";
 
-import {
-  BannerWrapper,
-  BannerImg,
-  BannerOverlay,
-  TextWrapper,
-  Text,
-} from "./InfoStyles";
+import { BannerWrapper, BannerImg, BannerOverlay, TextWrapper, Text } from "./InfoStyles";
 
 const Info = () => {
-  return (
-    <>
-      <BannerWrapper>
-        <BannerImg src="/assets/coding.jpg" alt="picture of code screen" />
-        <BannerOverlay></BannerOverlay>
-      </BannerWrapper>
-      <TextWrapper>
-        <Text>
-          Hey there, I'm Kayode, a creative and detailed-oriented software
-          developer with a passion for building state-of-the-art, user-friendly
-          websites and applications. I consider myself a problem solver who
-          loves taking on new and exciting challenges.
-        </Text>
+	return (
+		<>
+			<BannerWrapper>
+				<BannerImg src="/assets/coding.jpg" alt="picture of code screen" />
+				<BannerOverlay></BannerOverlay>
+			</BannerWrapper>
+			<TextWrapper>
+				<Text>
+					Hey there, I'm Kayode, a creative and detailed-oriented software developer with
+					a passion for building state-of-the-art, user-friendly websites and
+					applications. I consider myself a problem solver who loves taking on new and
+					exciting challenges.
+				</Text>
 
-        <Text>
-          I've worked on a wide range of web development projects and I'm
-          confident in my ability to build clean, efficient code that fulfils
-          project specifications. As a software developer, I am committed to
-          continuous learning and development. I recognise that the industry is
-          rapidly evolving, and I make it a point to keep current on the latest
-          trends and innovations. I am continuously seeking for new challenges
-          and ways to broaden my expertise.
-        </Text>
-      </TextWrapper>
-    </>
-  );
+				<Text>
+					I've worked on a wide range of web development projects and I'm confident in my
+					ability to build clean, efficient code that fulfils project specifications. As a
+					software developer, I am committed to continuous learning and development.
+				</Text>
+			</TextWrapper>
+		</>
+	);
 };
 
 export default Info;
--- components/projects/projectsList.js
+++ components/projects/projectsList.js
@@ -1,57 +1,46 @@
 const projects = [
-  {
-    title: "exploremoviez",
-    img: "/assets/exploremoviez.png",
-    description:
-      "A movie explorer website created with Reactjs. Movies are fetched from the TMDB Api.",
-    live: "//exploremoviez.netlify.com",
-    repo: "//github.com/mrphemi/exploremoviez",
-    stack: ["React", "Redux", "SCSS"],
-  },
-  {
-    title: "Flox",
-    description:
-      "flox empowers teams large and small to view and manage their entire software inventory.",
-    img: "/assets/flox.png",
-    live: "//floxdev.com",
-    repo: "",
-    stack: ["Next.js", "Tailwindcss", "Markdown", "Mdx"],
-  },
-  {
-    title: "True Global",
-    img: "/assets/true.png",
-    description: "Investment company based in London.",
-    live: "//true.global",
-    repo: "",
-    stack: ["Next.js", "Tailwindcss", "Storyblok CMS"],
-  },
-  {
-    title: "Studio Notion",
-    img: "/assets/studio-notion.png",
-    description:
-      "Studio notion is a media and content business based in the UK",
-    live: "//studionotion.com",
-    repo: "",
-    stack: ["Next.js", "Tailwindcss"],
-  },
-  {
-    title: "George Grace Represents",
-    img: "/assets/ggreps.png",
-    description:
-      "George Grace Represents is an illustration agency established in London in 2011.",
-    live: "//ggreps.com",
-    repo: "",
-    stack: ["Next.js", "Tailwindcss", "Storyblok CMS"],
-  },
-  {
-    title: "Badur Foundation",
-    img: "/assets/badur.png",
-    description:
-      "Badur Foundation is a registered charity in England and Wales.",
-    live: "//badurfoundation.org",
-    repo: "",
-    stack: ["Next.js", "Tailwindcss", "GraphQL", "DatoCMS"],
-  },
+	{
+		title: "Hold Storage",
+		img: "/assets/hold.jpg",
+		description: "Hold Storage is a storage company based in the UK.",
+		live: "//holdstorage.co.uk",
+		repo: "",
+		stack: ["Next.js", "Sanity CMS", "Tailwindcss"],
+	},
+	{
+		title: "True Global",
+		img: "/assets/true.png",
+		description: "Investment company based in London.",
+		live: "//true.global",
+		repo: "",
+		stack: ["Next.js", "Tailwindcss", "Storyblok CMS"],
+	},
+	{
+		title: "George Grace Represents",
+		img: "/assets/ggreps.png",
+		description:
+			"George Grace Represents is an illustration agency established in London in 2011.",
+		live: "//ggreps.com",
+		repo: "",
+		stack: ["Next.js", "Tailwindcss", "Storyblok CMS"],
+	},
+	{
+		title: "Badur Foundation",
+		img: "/assets/badur.png",
+		description: "Badur Foundation is a registered charity in England and Wales.",
+		live: "//badurfoundation.org",
+		repo: "",
+		stack: ["Next.js", "Tailwindcss", "GraphQL", "DatoCMS"],
+	},
+	{
+		title: "exploremoviez",
+		img: "/assets/exploremoviez.png",
+		description:
+			"A movie explorer website created with Reactjs. Movies are fetched from the TMDB Api.",
+		live: "//exploremoviez.netlify.com",
+		repo: "//github.com/mrphemi/exploremoviez",
+		stack: ["React", "Redux", "SCSS"],
+	},
 ];
 
 export default projects;
