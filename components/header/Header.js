14f981a186bb20f72449e1ab68871bf059b7e3fa
Femi <mrlapinho@gmail.com>
2024-10-26 23:15:25 +0100

    Update header

--- components/header/Header.js
+++ components/header/Header.js
@@ -2,57 +2,52 @@ import React from "react";
 import Link from "next/link";
 
 import {
-  ColoredText,
-  Logo,
-  LogoImg,
-  IconsWrapper,
-  Title,
-  HorizontalLine,
-  SocialWrapper,
-  TwitterIcon,
-  LinkedInIcon,
-  GithubIcon,
-  Wrapper,
+	ColoredText,
+	Logo,
+	LogoImg,
+	IconsWrapper,
+	Title,
+	HorizontalLine,
+	SocialWrapper,
+	TwitterIcon,
+	LinkedInIcon,
+	GithubIcon,
+	Wrapper,
 } from "./HeaderStyles";
 
 const Home = () => {
-  return (
-    <>
-      <Link href="/">
-        <a>
-          <LogoImg src="/assets/self_img.jpg" alt="my picture" />
-        </a>
-      </Link>
-      <Wrapper>
-        <SocialWrapper>
-          <HorizontalLine />
-          <IconsWrapper>
-            <Link href="//github.com/mrphemi">
-              <a target="_blank" rel="noopener noreferrer">
-                <GithubIcon />
-              </a>
-            </Link>
-            <Link href="//twitter.com/mrphemi">
-              <a target="_blank" rel="noopener noreferrer">
-                <TwitterIcon />
-              </a>
-            </Link>
-            <Link href="//linkedin.com/in/kayode-oluwafemi-b6918413b/">
-              <a target="_blank" rel="noopener noreferrer">
-                <LinkedInIcon />
-              </a>
-            </Link>
-          </IconsWrapper>
-        </SocialWrapper>
-        <Title>
-          hello i'm kayode <br />
-          software <ColoredText>developer</ColoredText>,
-          <br />
-          creative and tech enthusiast
-        </Title>
-      </Wrapper>
-    </>
-  );
+	return (
+		<>
+			<Link href="/">
+				<a>
+					<LogoImg src="/assets/self_img.jpg" alt="my picture" />
+				</a>
+			</Link>
+			<Wrapper>
+				<SocialWrapper>
+					<HorizontalLine />
+					<IconsWrapper>
+						<Link href="//github.com/mrphemi">
+							<a target="_blank" rel="noopener noreferrer">
+								<GithubIcon />
+							</a>
+						</Link>
+						<Link href="//linkedin.com/in/kayode-oluwafemi-b6918413b/">
+							<a target="_blank" rel="noopener noreferrer">
+								<LinkedInIcon />
+							</a>
+						</Link>
+					</IconsWrapper>
+				</SocialWrapper>
+				<Title>
+					hello, i'm Femi <br />
+					software <ColoredText>developer</ColoredText>,
+					<br />
+					Frontend Developer.
+				</Title>
+			</Wrapper>
+		</>
+	);
 };
 
 export default Home;
