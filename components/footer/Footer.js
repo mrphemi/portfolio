c4c65660754a26ea03d395182a4133442fb8ada3
Femi <mrlapinho@gmail.com>
2024-10-26 23:20:47 +0100

    update footer

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -4,41 +4,33 @@ import Link from "next/link";
 import Twitter from "../../svgs/twitter.svg";
 import Linkedin from "../../svgs/linkedin.svg";
 
-import {
-  Heading,
-  Text,
-  MailLink,
-  Wrapper,
-  SocialIconsWrapper
-} from "./FooterStyles";
+import { Heading, Text, MailLink, Wrapper, SocialIconsWrapper } from "./FooterStyles";
 
 const Footer = () => {
-  return (
-    <div>
-      <Heading>let's create beauty together</Heading>
-      <Text>
-        Feel free to reach out if you're looking for a <br /> developer, have a
-        question, or just want to connect.
-      </Text>
-      <Wrapper>
-        <MailLink href="mailto:fkayode94@gmail.com">
-          Fkayode94@gmail.com
-        </MailLink>
-        <SocialIconsWrapper>
-          <Link href="//twitter.com/mrphemi">
-            <a target="_blank" rel="noopener noreferrer">
-              <Twitter />
-            </a>
-          </Link>
-          <Link href="//linkedin.com/in/kayode-oluwafemi-b6918413b/">
-            <a target="_blank" rel="noopener noreferrer">
-              <Linkedin />
-            </a>
-          </Link>
-        </SocialIconsWrapper>
-      </Wrapper>
-    </div>
-  );
+	return (
+		<div>
+			<Heading>let's create beauty together</Heading>
+			<Text>
+				Feel free to reach out if you're looking for a <br /> developer, have a question, or
+				just want to connect.
+			</Text>
+			<Wrapper>
+				<MailLink href="mailto:fkayode94@gmail.com">Fkayode94@gmail.com</MailLink>
+				<SocialIconsWrapper>
+					<Link href="//github.com/mrphemi">
+						<a target="_blank" rel="noopener noreferrer">
+							<GithubIcon />
+						</a>
+					</Link>
+					<Link href="//linkedin.com/in/kayode-oluwafemi-b6918413b/">
+						<a target="_blank" rel="noopener noreferrer">
+							<Linkedin />
+						</a>
+					</Link>
+				</SocialIconsWrapper>
+			</Wrapper>
+		</div>
+	);
 };
 
 export default Footer;
